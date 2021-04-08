2cae25e666b99cbcf24925b98956cef8adc6fafb
MikeAttia <80725527+MikeAttia@users.noreply.github.com>
2021-04-08 12:19:13 -0400

    Add files via upload

---
+++ sumOfTwoarrays.js
@@ -0,0 +1,12 @@
+let num1=[1,2,3,4,5,6];
+let num2=[1,7,2,3,4,12];
+let num3=[];
+let sum=0;
+for(i=1;i<num1.length;i++){
+    sum=num1[i]+num2[i];
+    num3.push(sum)
+   
+}
+console.log(num3);
+
+
